ece3a2c1c76b944d25686dcdcf1c03cf9c18c12b
buisihung11 <38785848+buisihung11@users.noreply.github.com>
2020-05-26 19:45:52 +0700

    Remvoe unused - component

--- config/router.js
+++ config/router.js
@@ -227,31 +227,6 @@ export default [
             ],
           },
           {
-            name: 'editor',
-            icon: 'highlight',
-            path: '/editor',
-            routes: [
-              {
-                name: 'flow',
-                icon: 'smile',
-                path: '/editor/flow',
-                component: './editor/flow',
-              },
-              {
-                name: 'mind',
-                icon: 'smile',
-                path: '/editor/mind',
-                component: './editor/mind',
-              },
-              {
-                name: 'koni',
-                icon: 'smile',
-                path: '/editor/koni',
-                component: './editor/koni',
-              },
-            ],
-          },
-          {
             path: '/',
             redirect: '/dashboard/analysis',
             authority: ['admin', 'user'],
