e3915b9a6c926dadade13b8ffc75b33f91b73b00
buisihung11 <38785848+buisihung11@users.noreply.github.com>
2020-05-26 20:15:57 +0700

    Update router file remvoe dashboard

--- config/router.js
+++ config/router.js
@@ -41,31 +41,6 @@ export default [
         authority: ['admin', 'user'],
         routes: [
           {
-            path: '/dashboard',
-            name: 'dashboard',
-            icon: 'dashboard',
-            routes: [
-              {
-                name: 'analysis',
-                icon: 'smile',
-                path: '/dashboard/analysis',
-                component: './dashboard/analysis',
-              },
-              {
-                name: 'monitor',
-                icon: 'smile',
-                path: '/dashboard/monitor',
-                component: './dashboard/monitor',
-              },
-              {
-                name: 'workplace',
-                icon: 'smile',
-                path: '/dashboard/workplace',
-                component: './dashboard/workplace',
-              },
-            ],
-          },
-          {
             path: '/form',
             icon: 'form',
             name: 'form',
@@ -144,25 +119,7 @@ export default [
               },
             ],
           },
-          {
-            path: '/profile',
-            name: 'profile',
-            icon: 'profile',
-            routes: [
-              {
-                name: 'basic',
-                icon: 'smile',
-                path: '/profile/basic',
-                component: './profile/basic',
-              },
-              {
-                name: 'advanced',
-                icon: 'smile',
-                path: '/profile/advanced',
-                component: './profile/advanced',
-              },
-            ],
-          },
+
           {
             name: 'result',
             icon: 'CheckCircleOutlined',
@@ -204,25 +161,6 @@ export default [
                 icon: 'smile',
                 path: '/exception/500',
                 component: './exception/500',
-              },
-            ],
-          },
-          {
-            name: 'account',
-            icon: 'user',
-            path: '/account',
-            routes: [
-              {
-                name: 'center',
-                icon: 'smile',
-                path: '/account/center',
-                component: './account/center',
-              },
-              {
-                name: 'settings',
-                icon: 'smile',
-                path: '/account/settings',
-                component: './account/settings',
               },
             ],
           },
